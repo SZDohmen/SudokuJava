8a64dc4f3e45c7e7e98d60d32960f633ef04b49c
unknown <szdohmen@gmail.com>
2021-11-16 09:46:26 -0300

    project working v1.0

---
+++ src/sudoku/Sudoku.java
@@ -0,0 +1,12 @@
+
+package sudoku;
+
+import GameInterface.LevelInterface;
+
+public class Sudoku {
+    public static void main(String[] args) {
+        LevelInterface elegirNivel = new LevelInterface();
+        elegirNivel.setVisible(true);
+    }
+    
+}
